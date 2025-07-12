8bc99395d699960ee84d0b2b076af9f159aca515
0xMALIGNANT <0xbitbleed@gmail.com>
2025-07-12 08:30:49 -0400

    Update Pyramid.java

--- Pyramid.java
+++ Pyramid.java
@@ -7,3 +7,14 @@ public class Pyramid{
     System.out.print("Enter Your String: ");
     String pyramid_string = sc.nextLine();
     sc.close();
+
+    for(int line = 0; line <= pyramid_string.length() - 1; line++) {
+
+        for(int leadingSpaces = 0; leadingSpaces <= pyramid_string.length() - line; leadingSpaces++) {
+            System.out.print(" ");
+        }
+
+        for(int IndexPrint = 0; IndexPrint <= line; IndexPrint++) {
+            char printchar = pyramid_string.charAt(IndexPrint);
+            System.out.print(printchar + " ");
+        }
