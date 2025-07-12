cf44bb6c8f21336fe0d0f9984aecb5184d7f4a55
0xMALIGNANT <0xbitbleed@gmail.com>
2025-07-12 09:36:32 -0400

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 public class Calculator{
 
     String menu = "[0] - ADDITION \n" +
-                  "[1] - SUBRACTION \n" +;
+                  "[1] - SUBRACTION \n" +
+                  "[2] - MULTIPLICATION \n" +;
 
 
     public static void main(String[] args) {
