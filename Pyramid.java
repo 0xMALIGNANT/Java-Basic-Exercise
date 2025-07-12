14af1c7836eb5e827988a64a32f7c24f944ce66a
0xMALIGNANT <0xbitbleed@gmail.com>
2025-07-12 08:30:39 -0400

    Update Pyramid.java

--- Pyramid.java
+++ Pyramid.java
@@ -1,1 +1,9 @@
-Pyramid
+import java.util.Scanner;
+
+public class Pyramid{
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter Your String: ");
+    String pyramid_string = sc.nextLine();
+    sc.close();
